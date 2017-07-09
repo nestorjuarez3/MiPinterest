8ca162068af0c8cb35901bd2c9bc1d630c0afcc5
Nestor <nestoromarjuarez18@gmail.com>
2017-07-09 17:08:43 -0500

    Resource Pin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cars
+  resources :pins
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
